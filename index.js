ed141c2a9b8cbfbe8c5c2d033658da5d364b7995
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-05-23 02:34:18 +0600

    mongo db added

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+const verifyJwt = async (req,res,next) => {
+
+}
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wkgcv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect()
+        const carPartsCollection = client.db("spare-parts").collection("car-parts");
+        //console.log('db connected');
+        app.get('/car-parts',async(req,res)=>{
+            const result = await carPartsCollection.find({}).toArray();
+            res.send(result);
+        })
+    }
+    finally{
+        //await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send("wellcom to spare parts.!");
